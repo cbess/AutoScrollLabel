5ecfad81eac6379634adfa030fc971d40d24209d
C. Bess <cbess@quantumquinn.com>
2015-04-02 18:55:07 -0500

    - cleanup code; update docs; update screenshot
    - #45 has been implemented and updated

--- CBAutoScrollLabel/CBAutoScrollLabel.m
+++ CBAutoScrollLabel/CBAutoScrollLabel.m
@@ -33,7 +33,7 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
 @interface CBAutoScrollLabel ()
 
 @property (nonatomic, strong) NSArray *labels;
-@property (strong, nonatomic, readonly) UILabel *mainLabel;
+@property (nonatomic, strong, readonly) UILabel *mainLabel;
 @property (nonatomic, strong) UIScrollView *scrollView;
 
 @end
@@ -91,22 +91,17 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void) updateLabelsOnFrameChange {
-    [self refreshLabels];
-    [self applyGradientMaskForFadeLength:self.fadeLength enableFade:self.scrolling];
-}
-
 - (void)setFrame:(CGRect)frame {
     [super setFrame:frame];
     
-    [self updateLabelsOnFrameChange];
+    [self didChangeFrame];
 }
 
 // For autolayout
 - (void)setBounds:(CGRect)bounds {
     [super setBounds:bounds];
     
-    [self updateLabelsOnFrameChange];
+    [self didChangeFrame];
 }
 
 #pragma mark - Properties
@@ -278,7 +273,7 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
 
     // animate the scrolling
     NSTimeInterval duration = labelWidth / self.scrollSpeed;
-    [UIView animateWithDuration:duration delay:self.pauseInterval options:self.animationOptions | UIViewAnimationOptionAllowUserInteraction animations: ^{
+    [UIView animateWithDuration:duration delay:self.pauseInterval options:self.animationOptions | UIViewAnimationOptionAllowUserInteraction animations:^{
          // adjust offset
          self.scrollView.contentOffset = (doScrollLeft ? CGPointMake(labelWidth + self.labelSpacing, 0) : CGPointZero);
      } completion: ^(BOOL finished) {
@@ -298,18 +293,18 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
     __block float offset = 0;
 
     each_object(self.labels, ^(UILabel *label) {
-                    [label sizeToFit];
-
-                    CGRect frame = label.frame;
-                    frame.origin = CGPointMake(offset, 0);
-                    frame.size.height = CGRectGetHeight(self.bounds);
-                    label.frame = frame;
-
-                    // Recenter label vertically within the scroll view
-                    label.center = CGPointMake(label.center.x, roundf(self.center.y - CGRectGetMinY(self.frame)));
-
-                    offset += CGRectGetWidth(label.bounds) + self.labelSpacing;
-                });
+        [label sizeToFit];
+        
+        CGRect frame = label.frame;
+        frame.origin = CGPointMake(offset, 0);
+        frame.size.height = CGRectGetHeight(self.bounds);
+        label.frame = frame;
+        
+        // Recenter label vertically within the scroll view
+        label.center = CGPointMake(label.center.x, roundf(self.center.y - CGRectGetMinY(self.frame)));
+        
+        offset += CGRectGetWidth(label.bounds) + self.labelSpacing;
+    });
 
     self.scrollView.contentOffset = CGPointZero;
     [self.scrollView.layer removeAllAnimations];
@@ -323,7 +318,7 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
 
         EACH_LABEL(hidden, NO)
 
-        [self applyGradientMaskForFadeLength : self.fadeLength enableFade : self.scrolling];
+        [self applyGradientMaskForFadeLength:self.fadeLength enableFade:self.scrolling];
 
         [self scrollLabelIfNeeded];
     } else {
@@ -341,6 +336,12 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
 
         [self applyGradientMaskForFadeLength:0 enableFade:NO];
     }
+}
+
+// bounds or frame has been changeds
+- (void)didChangeFrame {
+    [self refreshLabels];
+    [self applyGradientMaskForFadeLength:self.fadeLength enableFade:self.scrolling];
 }
 
 #pragma mark - Gradient
@@ -388,6 +389,7 @@ static void each_object(NSArray *objects, void (^block)(id object)) {
         // apply calculations to mask
         gradientMask.locations = @[@0, leftFadePoint, rightFadePoint, @1];
 
+        // don't animate the mask change
         [CATransaction begin];
         [CATransaction setDisableActions:YES];
         self.layer.mask = gradientMask;
