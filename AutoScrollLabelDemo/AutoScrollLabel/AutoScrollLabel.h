cf999212c6235e90b8f102234559b01c593dfa3d
Kevin Delannoy <delannoyk@gmail.com>
2016-01-02 15:32:44 +0100

    Added AutoScrollLabel scheme in demo for Carthage

---
+++ AutoScrollLabelDemo/AutoScrollLabel/AutoScrollLabel.h
@@ -0,0 +1,19 @@
+//
+//  AutoScrollLabel.h
+//  AutoScrollLabel
+//
+//  Created by Kevin DELANNOY on 02/01/16.
+//  Copyright © 2016 C. Bess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AutoScrollLabel.
+FOUNDATION_EXPORT double AutoScrollLabelVersionNumber;
+
+//! Project version string for AutoScrollLabel.
+FOUNDATION_EXPORT const unsigned char AutoScrollLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoScrollLabel/PublicHeader.h>
+
+#import "CBAutoScrollLabel.h"
